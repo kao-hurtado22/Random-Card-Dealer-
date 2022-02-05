e79b8db6456a3e8f8e0657844b346c5a47865aeb
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-02-05 00:33:03 +0000

    Guardando Carta

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  document.querySelector(".card").className = `card ${generateRandomSuit()}`;
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spades", "hearts", "clubs"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
