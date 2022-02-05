21db74d8736a45cd530f56572b98922abc3031e7
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-02-05 01:18:16 +0000

    Actualizando carta

--- src/app.js
+++ src/app.js
@@ -1,30 +1,29 @@
 window.onload = () => {
+  let generateRandomNumber = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumbers];
+  };
+
+  let generateRandomSuit = () => {
+    let suit = ["diamond", "spades", "hearts", "clubs"];
+    let indexSuit = Math.floor(Math.random() * suit.length);
+    return suit[indexSuit];
+  };
   document.querySelector(".card").className = `card ${generateRandomSuit()}`;
   document.querySelector(".card").innerHTML = generateRandomNumber();
-};
-
-let generateRandomNumber = () => {
-  let numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let indexNumbers = Math.floor(Math.random() * numbers.length);
-  return numbers[indexNumbers];
-};
-
-let generateRandomSuit = () => {
-  let suit = ["diamond", "spades", "hearts", "clubs"];
-  let indexSuit = Math.floor(Math.random() * suit.length);
-  return suit[indexSuit];
 };
